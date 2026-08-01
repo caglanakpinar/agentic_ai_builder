93d019e1ee9d8d58208e517898738b33c99c5582
caglanakpinar <cakpinar23@gmail.com>
2026-08-01 09:33:07 +0300

    Update configs.py

--- uilts/configs.py
+++ uilts/configs.py
@@ -1,19 +1,39 @@
-
-
+from dataclasses import dataclass
 from logging import config
 from pathlib import Path
 import yaml
 
 
+@dataclass
+class LLMConfigs:
+    model_name: str
+    temperature: float
+    max_tokens: int
+    api_key: str
+    tools: list[dict[str, str]] | None = None
+    type: str = "generator" # options are "generator" or "retriever", "tool caller", "tool generator"
+    mcp_servers: list[str] | None = None
+
+
+@dataclass
 class VectorDBConfigs:
-    def __init__(self, host: str, port: int, api_key: str, path: str):
-        self.host = host
-        self.port = port
-        self.api_key = api_key
-        self.path = path
+    host: str
+    port: int
+    api_key: str
+    path: str
+
+
+@dataclass
+class EmbeddingsConfigs:
+    model_name: str
+    api_key: str
+
 
 class Configs:
     db_confgs: dict[str, VectorDBConfigs] = {}
+    llm_configs: dict[str, LLMConfigs] = {}
+    embeddings_configs: dict[str, EmbeddingsConfigs] = {}
+
     def __init__(self, current_filename: str):
         self.current_dir = Path(__file__).parent  / current_filename
         self.read_yaml()
@@ -41,4 +61,23 @@ class Configs:
                         api_key=self.dbs.get('api_key', ''),
                         path=self.dbs.get('path', '')
                     )
- 
+
+    def llm_configer(self):
+        if 'llm' in self.__dict__:
+            for llm_name in self.llm:
+                self.llm_configs[llm_name] = LLMConfigs(
+                    model_name=self.llm.get('model_name', ''),
+                    temperature=self.llm.get('temperature', 0.0),
+                    max_tokens=self.llm.get('max_tokens', 0),
+                    api_key=self.llm.get('api_key', ''),
+                    type=self.llm.get('type', 'generator'),
+                    tools=self.llm.get('tools', None)
+                )
+
+    def embeddings_configer(self):
+        if 'embeddings' in self.__dict__:
+            for embedding_name in self.embeddings:
+                self.embeddings_configs[embedding_name] = EmbeddingsConfigs(
+                    model_name=self.embeddings.get('model_name', ''),
+                    api_key=self.embeddings.get('api_key', '')
+                )
